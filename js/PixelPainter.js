80c83d9f35463efd7e578063ab490bf76d613f79
Pamela Yang <PamelaYang@Pamelas-MacBook-Pro.local>
2016-03-18 19:49:16 -1000

    basicsCompleted

---
+++ js/PixelPainter.js
@@ -0,0 +1,52 @@
+function pixelpainter() {
+
+  var storecolor = null;
+
+  document.getElementById('pink').addEventListener('click', function(){
+     storecolor = 'pink';
+  });
+  document.getElementById('blue').addEventListener('click', function(){
+     storecolor = 'blue';
+  });
+  document.getElementById('purple').addEventListener('click', function(){
+     storecolor = 'purple';
+  });
+  document.getElementById('green').addEventListener('click', function(){
+     storecolor = 'green';
+  });
+
+  document.getElementById('eraseBtn').addEventListener('click', function(){
+     storecolor = 'white';
+  });
+
+  document.getElementById('clearBtn').addEventListener('click', clearAll);
+
+  var changer = document.querySelectorAll('td');
+
+  for(var i = 0; i < changer.length; i++) {
+    changer[i].addEventListener('click', changeColor);
+  }
+
+  function changeColor(event) {
+    this.className = '';
+    this.className  = storecolor;
+    }
+
+  function clearAll(event) {
+    for(var i = 0; i < changer.length; i++) {
+      changer[i].className = '';
+    }
+  }
+
+
+
+
+
+
+return {
+
+
+};
+}
+
+var pixelpainter = pixelpainter();
