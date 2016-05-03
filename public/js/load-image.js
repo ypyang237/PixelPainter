a9a1378abcdbe3300b4d18530e0d370e736b8abb
ypyang237 <ypyang237@gmail.com>
2016-05-03 12:16:20 -1000

    finish load-image xml request to reload saved image

--- public/js/load-image.js
+++ public/js/load-image.js
@@ -7,10 +7,15 @@ loadBtn.addEventListener('click', function(event) {
 
 function loadImage() {
 
+
+
 var loadReq = new XMLHttpRequest();
   loadReq.addEventListener('load', function(data) {
     var mapToImage = JSON.parse(data.target.responseText);
-    console.log(mapToImage)
+    var imageInstructions = JSON.parse(mapToImage.imageState);
+    for(var i in imageInstructions) {
+      document.getElementById(i).style.background = imageInstructions[i];
+    }
   });
   loadReq.open("GET", "/image");
   loadReq.send();
