0769f76c91e9bbd00717f47e81b012398618f9fe
ypyang237 <ypyang237@gmail.com>
2016-05-03 18:09:31 -1000

    finishes save to database function

--- public/js/save-image.js
+++ public/js/save-image.js
@@ -10,27 +10,19 @@ function saveImage() {
   var divStates = {};
   var targetDiv;
   var targetDivColor;
+  var name = document.getElementById('saveInput').value;
 
   Array.prototype.forEach.call(allDivs, function(element) {
       targetDiv = element.id;
       targetDivColor = element.style.background;
       divStates[targetDiv] = targetDivColor;
   });
-
   var saveReq = new XMLHttpRequest();
   saveReq.addEventListener('load', function(data) {
-    console.log(data);
+    console.log(data); //data is 'successPost'
   })
   saveReq.open("POST", "/image");
   saveReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  saveReq.send("imageState=" + JSON.stringify(divStates));
-
-
-
-
-
-
-
-
+  saveReq.send("imageState=" + JSON.stringify(divStates) + "&name=" + name);
 
 }
