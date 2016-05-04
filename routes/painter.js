a1ef8eed93b1a13514164517baa56e44926a3b7b
ypyang237 <ypyang237@gmail.com>
2016-05-03 15:26:47 -1000

    creates empty image route for modularizing in the future

--- routes/painter.js
+++ routes/painter.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const express = require('express'),
-      router = express.Router()
+      router = express.Router(),
+      paintSchema = require('../server')
       ;
 
+router.route('/image')
+  .post(function(req, res) {
+
+    // var image = new paintSchema({
+    //   // name: req.body.name
+    //   imageState: req.body.imageInstructions
+    // })
+  })
